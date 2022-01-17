37d32d60dda1629e41419dacd3eb4490539659af
rafi <rafi.alfatih@gmail.com>
2022-01-17 14:08:27 +0700

    fix navbar dropdown

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,53 +1,70 @@
 <template>
-        <section class="about mt-3 pt-3">
-        <div class="container">
-            <div class="row">
-                <div class="col-md-6 order-1" data-aos="zoom-in" data-aos-delay="600">
-                    <img src="../assets/about.png" class="img-fluid"  alt="">
-                </div>
-                <div class="col-md-6 order-md-2 pt-3 my-auto" data-aos="fade-up" data-aos-delay="300">
-                    <h2 class="font-weight-bold">Kopi Nikmat <span class="text-theme">Harga Merakyat</span></h2>
-                    <p class="text-muted mt-3">Warung Kopi yang menyediakan harga termurah di Rambipuji. dengan layanan terbaik dan suasana tempat yang nyaman, membuat anda betah untuk berlama - lama di warung kami.</p>
-                    <div class="row mt-4">
-                        <div class="col-md-6">
-                            <h3 class="text-theme font-weight-bold">
-                                2
-                            </h3>
-                            <h4 class="font-weight-bold">Barista</h4>
-                            <p class="text-muted">Para Barista yang sudah memiliki pengalaman puluhan tahun dalam membuat kopi yang digemari pelanggan.</p>
-                        </div>
-                        <div class="col-md-6">
-                            <h3 class="text-theme font-weight-bold">
-                                100++
-                            </h3>
-                            <h4 class="font-weight-bold">Pelanggan</h4>
-                            <p class="text-muted">Ratusan pelanggan berdatangan setiap harinya, menanti Kopi racikan Bu Musliha.</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-            <div class="row mt-3">
-                <div class="col-md-6 mt-3 my-auto" data-aos="fade-up" data-aos-delay="300">
-                    <h1 class="font-weight-bold t-3">
-                        Apakah anda suka bermain Catur?
-                    </h1>
-                    <p class="text-muted">Selain menikmati hidangan aneka racikan kopi, kamu juga bisa melatih kemampuan dalam bermain catur. terdapat ribuan pertandingan setiap harinya dan rasakan sensasi kompetisi</p>
-                    <a href="#" class="btn btn-color-theme pr-4 pl-4 pt-2 pb-2 t-3">
-                        Kunjungi Sekarang
-                    </a>
-                </div>
-                <div class="col-md-6" data-aos="zoom-in" data-aos-delay="600">
-                    <img src="../assets/about2.png" class="img-fluid"  alt="">
-                </div>
-            </div>
+  <section class="about mt-3 pt-3">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-6 order-1" data-aos="zoom-in" data-aos-delay="600">
+          <img src="../assets/about.png" class="img-fluid" alt="" />
         </div>
-    </section>
+        <div
+          class="col-md-6 order-md-2 pt-3 my-auto"
+          data-aos="fade-up"
+          data-aos-delay="300"
+        >
+          <h2 class="font-weight-bold">
+            Kopi Nikmat <span class="text-theme">Harga Merakyat</span>
+          </h2>
+          <p class="text-muted mt-3">
+            Warung Kopi yang menyediakan harga termurah di Rambipuji. dengan
+            layanan terbaik dan suasana tempat yang nyaman, membuat anda betah
+            untuk berlama - lama di warung kami.
+          </p>
+          <div class="row mt-4">
+            <div class="col-md-6">
+              <h3 class="text-theme font-weight-bold">2</h3>
+              <h4 class="font-weight-bold">Barista</h4>
+              <p class="text-muted">
+                Para Barista yang sudah memiliki pengalaman puluhan tahun dalam
+                membuat kopi yang digemari pelanggan.
+              </p>
+            </div>
+            <div class="col-md-6">
+              <h3 class="text-theme font-weight-bold">100++</h3>
+              <h4 class="font-weight-bold">Pelanggan</h4>
+              <p class="text-muted">
+                Ratusan pelanggan berdatangan setiap harinya, menanti Kopi
+                racikan Bu Musliha.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="row mt-3">
+        <div
+          class="col-md-6 mt-3 my-auto"
+          data-aos="fade-up"
+          data-aos-delay="300"
+        >
+          <h1 class="font-weight-bold t-3">Apakah anda suka bermain Catur?</h1>
+          <p class="text-muted">
+            Selain menikmati hidangan aneka racikan kopi, kamu juga bisa melatih
+            kemampuan dalam bermain catur. terdapat ribuan pertandingan setiap
+            harinya dan rasakan sensasi kompetisi
+          </p>
+          <a href="#" class="btn btn-color-theme pr-4 pl-4 pt-2 pb-2 t-3">
+            Kunjungi Sekarang
+          </a>
+        </div>
+        <div class="col-md-6" data-aos="zoom-in" data-aos-delay="600">
+          <img src="../assets/about2.png" class="img-fluid" alt="" />
+        </div>
+      </div>
+    </div>
+  </section>
 </template>
 
 <script>
-import 'bootstrap/dist/css/bootstrap.css';
-export default{
-    name: 'About',
-}
+export default {
+  name: "About",
+};
 </script>
